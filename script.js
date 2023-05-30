13f2b33b4e35123cd90181884e2d0716b3eb85ce
Wallacy <wallacy_vinicius@hotmail.com>
2023-05-30 02:38:14 -0300

    aula-condicionais-II

---
+++ script.js
@@ -0,0 +1,117 @@
+// let idade = 18
+
+// ifs aninhados
+
+// if(idade >= 13){
+
+//     if(idade <= 17 ){
+
+//     console.log("A pessoa dependente já pode ter um cartão de crédito vinculado ao seu");
+//     }else{
+
+//     console.log("Idade maior ou igual o 18"); 
+// }
+
+// }else{
+
+//     console.log("Consulte outras possibilidades do Labank");
+// }
+
+// Operadores lógicos
+
+// if(idade >= 13 && idade <= 17){
+//     console.log("A pessoa dependente já pode ter um cartão de crédito vinculado ao seu");
+
+// }else{
+//     console.log("Consulte outras possibilidades do Labank");
+
+// }
+
+// const possuiConta = prompt("Você já possui conta no Labank ? Responda sim ou não")
+
+// if(possuiConta.toLocaleLowerCase() === "sim"){
+//     console.log("Boas vindas!");
+
+// }else{
+//     console.log("Faça já o seu cadastro");
+
+// }
+
+
+//if ternário
+
+// possuiConta.toLocaleLowerCase() === "sim" ?
+//     console.log("Boas Vindas!") :
+//     console.log("faça já o seu cadastro!")
+
+// Switch case
+
+// switch(ramal){
+//     case 1:
+//         cartao = "Easy"
+//         break;
+//     case 2:
+//         cartao = "Black"
+//         break;
+//     case 3:
+//         cartao = "platinum"
+//         break;
+//     case 4:
+//         cartao = "Master Gold"
+//         break;
+//     default:
+//         console.log("Escolha uma das 4 opções disponiveis");
+// }
+
+
+
+let numero = Number(prompt("Digite um número"))
+
+// if(numero % 2 === 0){
+//      console.log(`O é divisivel por 2.`);
+
+//     if(numero % 3 === 0){
+//         console.log(`O é divisivel por 3.`);
+
+//     }else{
+//         console.log(`O não é divisivel por 2 e 3.`);
+
+//     }
+
+// }else{
+//     console.log(`Coloque outro número`);
+
+// }
+
+// if(numero % 2 === 0 && numero % 3 === 0){
+//     console.log("É divisivel por 2 e 3");
+
+// }else{
+//     console.log("Não é divisivel por 2 e 3");
+
+// }
+
+
+
+// const verificaNumero = prompt("Informe um número: ")
+// let frase
+// switch(verificaNumero){
+//     case 6:
+//         frase = "aaaaaaaaa"
+//         break;
+//     case 12:
+//         frase = "bbbbbbbbb"
+//         break;
+//     case 18:
+//         frase = "ccccccccc"
+//         break;
+//     case 24:
+//         frase = "ddddddddd"
+//         break;
+//     case 30:
+//         frase = "eeeeeeeee"
+//         break;
+//     default:
+//         console.log("O número é maior que 30 ou menor que 6");
+//     }
+    
